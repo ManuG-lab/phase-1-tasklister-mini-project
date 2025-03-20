558d1717ae944335c3790a78134e3dde1d7486e8
Emmanuel Gitau <gmannu2055@gmail.com>
2025-03-20 11:32:16 +0300

    initialize commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  taskForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const newTask = document.querySelector("#new-task-description").value;
+
+   
+    const taskItem = document.createElement("li");
+    taskItem.textContent = newTask;
+
+    
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "x";
+    deleteButton.style.marginLeft = "10px";
+    deleteButton.addEventListener("click", function () {
+      taskItem.remove();
+    });
+
+    
+    taskItem.appendChild(deleteButton);
+    taskList.appendChild(taskItem);
+
+    
+    taskForm.reset();
+  });
 });
